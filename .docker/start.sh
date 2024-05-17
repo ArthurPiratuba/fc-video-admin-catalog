62b9b78ca0ce8403b75e880bc9c4fbde6d4dea55
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-05-17 13:25:27 -0300

    Moved test file

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,4 +1,7 @@
 #1/bin/bash
-
+mkdir -p ~/.gnupg
+echo "use-agent" > ~/.gnupg/gpg.conf
+echo "extra-socket /home/vscode/.gnupg/S.gpg-agent" >> ~/.gnupg/gpg.conf
+echo "pinentry-program /usr/bin/pinentry-curses" >> ~/.gnupg/gpg-agent.conf
 npm install
 tail -f /dev/null
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:20.5.1-slim
-
+ 
 USER node
 
 WORKDIR /home/node/app
